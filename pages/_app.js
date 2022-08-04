9541d00cc2c7363e36572aec3601ff9114979253
Reuban Bryenton <r.bryenton2003@gmail.com>
2022-08-04 16:02:35 +0200

    Init + Introduction Page

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,12 @@ import 'nextra-theme-docs/style.css'
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+/*
+META.JSON
+
+"get-started": "Get Started",
+    "jobs": "Jobs",
+    "themes": "Themes",
+    "advanced": "Advanced"
+
+*/
