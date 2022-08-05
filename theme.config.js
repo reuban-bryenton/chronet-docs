4be396531f29edab3627ec0c32ab61a48c021a94
Reuban Bryenton <r.bryenton2003@gmail.com>
2022-08-05 22:50:01 +0200

    Sociam Image change + name changes

--- theme.config.js
+++ theme.config.js
@@ -1,60 +1,60 @@
 export default {
-    github: 'https://github.com/reuban_bryenton/chronet-docs',
-    docsRepositoryBase: 'https://github.com/reuban_bryenton/chronet-docs/blob/master',
+    github: 'https://github.com/reuban-bryenton/chronet-docs',
+    docsRepositoryBase: 'https://github.com/reuban-bryenton/chronet-docs/blob/master',
     titleSuffix: ' – Docs',
     logo: (
         <>
-        <span className="mr-2 font-extrabold hidden md:inline">Chronet.ca</span>
-        <span className="text-gray-600 font-normal hidden md:inline">
-            A chronet remake + remaster
-        </span>
+            <span className="mr-2 font-extrabold hidden md:inline">Chronet.ca</span>
+            <span className="text-gray-600 font-normal hidden md:inline">
+                A chronet remake + remaster
+            </span>
         </>
     ),
     head: (
         <>
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
-        <meta name="description" content="Chronet.ca: A chronet remake + remaster" />
-        <meta name="og:description" content="Chronet.ca: A chronet remake + remaster" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-        <meta name="twitter:site:domain" content="chronet-docs.vercel.app" />
-        <meta name="twitter:url" content="https://chronet-docs.vercel.app" />
-        <meta name="og:title" content="Nextra: Next.js static site generator" />
-        <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-        <meta name="apple-mobile-web-app-title" content="Nextra" />
-        <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-icon-180x180.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/android-icon-192x192.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/favicon-96x96.png"
-        />
-        <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-        />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta httpEquiv="Content-Language" content="en" />
+            <meta name="description" content="Chronet.ca: A chronet remake + remaster" />
+            <meta name="og:description" content="Chronet.ca: A chronet remake + remaster" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:image" content="https://imgur.com/6TlxG5f" />
+            <meta name="twitter:site:domain" content="chronet-docs.vercel.app" />
+            <meta name="twitter:url" content="https://chronet-docs.vercel.app" />
+            <meta name="og:title" content="Nextra: Next.js static site generator" />
+            <meta name="og:image" content="https://imgur.com/6TlxG5f" />
+            <meta name="apple-mobile-web-app-title" content="Nextra" />
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-icon-180x180.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="192x192"
+                href="/android-icon-192x192.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="96x96"
+                href="/favicon-96x96.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         </>
     ),
     search: true,
@@ -63,5 +63,5 @@ export default {
     footer: true,
     footerEditLink: 'Edit this page on GitHub',
     footerText: <>MIT {new Date().getFullYear()} © Chronet.ca.</>,
-    unstable_faviconGlyph: '👋',
+    unstable_faviconGlyph: '📋',
 }
